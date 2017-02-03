4a67e7b325f439c6170e764c6af9dfd2ade543df
yekuigood <yekuigood@163.com>
2017-02-03 16:51:51 +0800

    完善学生成绩系统

--- student.c
+++ student.c
@@ -26,7 +26,43 @@ void init_stu_data(void)
 void get_stu_data(void){
 	FILE *fp;
 	int ninzu = 0;	 /* 人数 */
-	char name[100];	 /* 姓名 */
+	char name[20];	 /* 姓名 */
+	char stuno[20];	 /* 学号 */
+	double height,weight,chinese,math,english; /* 身高，体重，语文，数学，英语 */
+	double hsum = 0.0 ;/* 身高合计 */
+	double wsum = 0.0 ;/* 体重合计 */
+	double csum = 0.0 ; /* 语文成绩全合计 */
+	double msum = 0.0 ; /*数学成绩合计*/
+	double esum = 0.0 ;/* 英语成绩合计 */
+	double total = 0.0;
+	
+	if((fp=fopen(stu_data_file,"r"))==NULL)
+		printf("\aFile open failed!\n");
+	else{
+		printf("Name    StuNo    Height    Weight    Chinese    Math    English    Total\n");
+		fscanf(fp,"%s%s%lf%lf%lf%lf%lf",name,stuno,&height,&weight,&chinese,&math,&english);
+		total = chinese+math+english;
+		//printf("%s  %s  %f  %f  %f  %f  %f  %f\n",name,stuno,height,weight,chinese,math,english,total);
+		printf("Name: %s\n",name);
+		printf("StuNo: %s\n",stuno);
+		printf("Height: %4.1f\n",height);
+		printf("Weight: %4.1f\n",weight);
+		printf("Chinese: %4.1f\n",chinese);
+		printf("Math: %4.1f\n",math);
+		printf("English: %4.1f\n",english);
+		printf("Total: %4.1f\n",total);
+		// while(==7){
+			//printf("%10s%10s%6.1f%6.1f%6.1f%6.1f%6.1f%6.1f\n",name,stuno,height,weight,chinese,math,english,chinese+math+english);
+		// }
+		//fclose(fp);
+	}
+}
+
+/* ---添加学生数据--- */
+void add_stu_data(){
+	FILE *fp;
+	int ninzu = 0;	 /* 人数 */
+	char name[20];	 /* 姓名 */
 	char stuno[20];	 /* 学号 */
 	double height,weight,chinese,math,english; /* 身高，体重，语文，数学，英语 */
 	double hsum = 0.0 ;/* 身高合计 */
@@ -35,18 +71,30 @@ void get_stu_data(void){
 	double msum = 0.0 ; /*数学成绩合计*/
 	double esum = 0.0 ;/* 英语成绩合计 */
 	
-	if((fp=fopen(stu_data_file,"r"))==NULL)
-		printf("\a File open failed!\n");
+	if((fp=fopen(stu_data_file,"a"))==NULL)
+		printf("\aFile open failed!\n");
 	else{
-		while(fscanf(fp,"%s%s%lf%lf%lf%lf%lf",name,stuno,&height,&weight,&chinese,&math,&english)==7){
-			
-		}
+		printf("Please input student name:");
+		scanf("%s",name);
+		printf("Please input student No:");
+		scanf("%s",stuno);
+		printf("Please input student Height:");
+		scanf("%lf",&height);
+		printf("Please input student Weight:");
+		scanf("%lf",&weight);
+		printf("Please input student Chinese:");
+		scanf("%lf",&chinese);
+		printf("Please input student Math:");
+		scanf("%lf",&math);
+		printf("Please input student English:");
+		scanf("%lf",&english);
+		// TODO
+		//1.检测数据合法性
+		//2.判断学生是否存在
+		//3.如果存在根据用户选择更新，放弃，重新输入
+		fprintf(fp,"%s%s%lf%lf%lf%lf%lf",name,stuno,height,weight,chinese,math,english);
+		fclose(fp);
 	}
-}
-
-/* ---添加学生数据--- */
-int add_stu_data(student stu){
-	return 0;
 }
 
 /* ---删除学生信息---*/
@@ -61,13 +109,41 @@ int exsit_stu(student stu)
 }
 
 /* ---学生排序--- */
-student[] sort_stu_data(void)
+void sort_stu_data(void)
 {
-	return StuDat;
+
 }
 
 int main(void)
 {
-
+	int slct;
+	printf(" Welcome to student sorce system!\n");
+	while(1){
+		printf("\n------------------------------Option Menu----------------------------\n");
+		printf(" 1.Input 1 to show student's sorce.\n");
+		printf(" 2.Input 2 to add student's sorce.\n");	
+		printf(" 3.Input 3 to update student's sorce.\n");
+		printf(" 4.Input 4 to delete student's sorce.\n");
+		printf(" 5.Input 5 to sort and show student's sorce.\n");
+		printf(" 0.Input 0 to quit student sorce system.\n");
+		printf("------------------------------End Menu--------------------------------\n\n");
+		scanf("%d",&slct);
+		if(slct==0){
+			printf("Quit student sorce system!\n");
+			break;
+		}else if(slct==1){
+			printf("Show student's sorce!\n");
+			get_stu_data();
+		}else if(slct==2){
+			printf("Add student's sorce!\n");
+			add_stu_data();
+		}else if(slct==3){
+			printf("Add student's sorce!\n");
+		}else if(slct==4){
+			printf("Delete student's sorce!\n");
+		}else if(slct==5){
+			printf("Sort student's sorce!\n");
+		}
+	}
 	return 0;
 }
